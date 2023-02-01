12bde546710a19d1a86eb70294b24d867ffc6dbb
Marcelo <marcelo_4940@hotmail.com>
2023-02-01 02:55:17 -0300

    Traduções

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,62 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ViewsModule } from './views/views.module';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppComponent } from './app.component';
 import { AppRouting } from './app.routing';
 import { SharedModule } from './shared/shared.module';
+
+export const ACCEPT_LANGUAGES: string[] = ['pt', 'en', 'es'];
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
     ViewsModule,
     AppRouting,
     BrowserAnimationsModule,
     SharedModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: AppInitializerFactory,
+      deps: [TranslateService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
+
+export function AppInitializerFactory(translate: TranslateService) {
+  return () => {
+    let navigatorLanguage = navigator.language;
+    let localeLanguage = ACCEPT_LANGUAGES.find((language) => {
+      return navigatorLanguage.includes(language);
+    });
+    if (!localeLanguage) localeLanguage = 'en';
+    translate.setDefaultLang(localeLanguage);
+    return translate.use(localeLanguage);
+  };
+}
